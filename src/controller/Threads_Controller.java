6512818ab4643a0265827fb8e2029639d94d3dbd
Gabriel Ribeiro <chucrutsgabriel@gmail.com>
2025-03-26 02:39:10 -0300

    refactor

--- src/controller/Threads_Controller.java
+++ src/controller/Threads_Controller.java
@@ -2,22 +2,21 @@ package controller;
 
 import java.util.concurrent.Semaphore;
 
-
 public class Threads_Controller extends Thread {
-	
+
 	private int idAviao;
 	private Semaphore semaforo;
 	String pistaAtual = escolhePista();
-	
+
 	public Threads_Controller(int idAviao, Semaphore semaforo) {
 		this.idAviao = idAviao;
 		this.semaforo = semaforo;
 	}
-	
+
 	public void run() {
-		
+
 		estManobrando();
-		//==============inicio da sessão critica===============//
+		// ==============inicio da sessão critica===============//
 		try {
 			semaforo.acquire();
 			estTaxiando();
@@ -26,85 +25,74 @@ public class Threads_Controller extends Thread {
 			e.printStackTrace();
 		} finally {
 			semaforo.release();
-		//==============fim da sessão critica===============//
+			// ==============fim da sessão critica===============//
 			estDecolando();
 		}
-		
-		
+
 	}
 
 	private void estManobrando() {
-		
-		 int tempo = (int) ((Math.random() * 401) + 300); // 300 a 700 milisegundos
-		
-		
-		
-		 try {
+
+		int tempo = (int) ((Math.random() * 401) + 300); // 300 a 700 milisegundos
+
+		try {
 			Thread.sleep(tempo);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		 System.out.println("O avião "+ idAviao +" esta manobrando na pista "+ pistaAtual);
+		System.out.println("O avião " + idAviao + " esta manobrando na pista " + pistaAtual);
 	}
-	
 
 	private void estTaxiando() {
 		int tempo = (int) ((Math.random() * 501) + 500); // 500 a 1000 milisegundos
-		
-		
-		 try {
+
+		try {
 			Thread.sleep(tempo);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		 System.out.println("O avião "+ idAviao +" esta taxiando na pista " + pistaAtual);
+		System.out.println("O avião " + idAviao + " esta taxiando na pista " + pistaAtual);
 	}
-		
 
-	
 	private void estAfastando() {
 		int tempo = (int) ((Math.random() * 501) + 300); // 500 a 800 milisegundos
-		
-		
-		 try {
+
+		try {
 			Thread.sleep(tempo);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		 System.out.println("O avião "+ idAviao +" esta afastando na pista " + pistaAtual);
-		
-	}
+		System.out.println("O avião " + idAviao + " esta afastando na pista " + pistaAtual);
 
-	
+	}
 
 	private void estDecolando() {
-		int tempo = (int) ((Math.random() *201) + 600); // 600 a 800 milisegundos
-		
-		
-		 try {
+		int tempo = (int) ((Math.random() * 201) + 600); // 600 a 800 milisegundos
+
+		try {
 			Thread.sleep(tempo);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		 System.out.println("O avião "+ idAviao +" esta decolando na pista "+ pistaAtual);
-		
+		System.out.println("O avião " + idAviao + " esta decolando na pista " + pistaAtual);
+
 	}
-	
+
 	private String escolhePista() {
-		 int pista = (int)(Math.random() * 2);
-		 
-		 if (pista == 0) {
-			 String pistaAtual = "norte";
-			 return pistaAtual;
-		 } else {
-			 String pistaAtual = "sul";
-			 return pistaAtual;
-		 }
-		
+		int pista = (int) (Math.random() * 2);
+
+		if (pista == 0) {
+			String pistaAtual = "norte";
+			return pistaAtual;
+		} else {
+			String pistaAtual = "sul";
+			return pistaAtual;
+		}
+
 	}
 
 }
--- src/view/Threads_Semaforos.java
+++ src/view/Threads_Semaforos.java
@@ -2,18 +2,16 @@ package view;
 
 import java.util.concurrent.Semaphore;
 import controller.Threads_Controller;
-import java.util.Random;
 
 public class Threads_Semaforos {
 	public static void main(String[] args) {
-		
-		
+
 		int permissoes = 2;
 		Semaphore semaforo = new Semaphore(permissoes);
-		
-		for(int idAviao = 1; idAviao < 13 ; idAviao++) {
+
+		for (int idAviao = 1; idAviao < 13; idAviao++) {
 			Thread tAviao = new Threads_Controller(idAviao, semaforo);
 			tAviao.start();
 		}
 	}
-} 
+}
