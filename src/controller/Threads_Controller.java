d6aee9ed1632dc6e319b5c95d7bdca0e60f7bc41
Gabriel Ribeiro <chucrutsgabriel@gmail.com>
2025-03-26 02:34:06 -0300

    feat func pistaAtual() para identificação do avião

--- src/controller/Threads_Controller.java
+++ src/controller/Threads_Controller.java
@@ -2,13 +2,12 @@ package controller;
 
 import java.util.concurrent.Semaphore;
 
+
 public class Threads_Controller extends Thread {
 	
 	private int idAviao;
 	private Semaphore semaforo;
-	
-	
-	
+	String pistaAtual = escolhePista();
 	
 	public Threads_Controller(int idAviao, Semaphore semaforo) {
 		this.idAviao = idAviao;
@@ -16,6 +15,7 @@ public class Threads_Controller extends Thread {
 	}
 	
 	public void run() {
+		
 		estManobrando();
 		//==============inicio da sessão critica===============//
 		try {
@@ -36,37 +36,45 @@ public class Threads_Controller extends Thread {
 	private void estManobrando() {
 		
 		 int tempo = (int) ((Math.random() * 401) + 300); // 300 a 700 milisegundos
+		
+		
+		
 		 try {
 			Thread.sleep(tempo);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		 System.out.println("O avião "+ idAviao +" esta manobrando");
+		 System.out.println("O avião "+ idAviao +" esta manobrando na pista "+ pistaAtual);
 	}
 	
+
 	private void estTaxiando() {
 		int tempo = (int) ((Math.random() * 501) + 500); // 500 a 1000 milisegundos
+		
+		
 		 try {
 			Thread.sleep(tempo);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		 System.out.println("O avião "+ idAviao +" esta taxiando");
+		 System.out.println("O avião "+ idAviao +" esta taxiando na pista " + pistaAtual);
 	}
 		
 
 	
 	private void estAfastando() {
 		int tempo = (int) ((Math.random() * 501) + 300); // 500 a 800 milisegundos
+		
+		
 		 try {
 			Thread.sleep(tempo);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		 System.out.println("O avião "+ idAviao +" esta afastando");
+		 System.out.println("O avião "+ idAviao +" esta afastando na pista " + pistaAtual);
 		
 	}
 
@@ -74,13 +82,28 @@ public class Threads_Controller extends Thread {
 
 	private void estDecolando() {
 		int tempo = (int) ((Math.random() *201) + 600); // 600 a 800 milisegundos
+		
+		
 		 try {
 			Thread.sleep(tempo);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		 System.out.println("O avião "+ idAviao +" esta decolando");
+		 System.out.println("O avião "+ idAviao +" esta decolando na pista "+ pistaAtual);
+		
+	}
+	
+	private String escolhePista() {
+		 int pista = (int)(Math.random() * 2);
+		 
+		 if (pista == 0) {
+			 String pistaAtual = "norte";
+			 return pistaAtual;
+		 } else {
+			 String pistaAtual = "sul";
+			 return pistaAtual;
+		 }
 		
 	}
 
